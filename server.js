a199c63f60ab5b09e151095fb6b4cc88da64db8c
minhtam <minhtamnhgp03@gmail.com>
2024-11-01 21:07:50 +0700

    thay cong

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
 // Connect database
@@ -13,7 +13,7 @@ const app = express();
       useCreateIndex: true,
       useFindAndModify: false,
     });
-    console.log('MongoDB Connected...');
+    console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
     // Exit process with failure
@@ -25,24 +25,29 @@ const app = express();
 app.use(express.json({ extended: false }));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/boards', require('./routes/api/boards'));
-app.use('/api/lists', require('./routes/api/lists'));
-app.use('/api/cards', require('./routes/api/cards'));
-app.use('/api/checklists', require('./routes/api/checklists'));
-app.use('/api/notifications', require('./routes/api/notifications'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/boards", require("./routes/api/boards"));
+app.use("/api/lists", require("./routes/api/lists"));
+app.use("/api/cards", require("./routes/api/cards"));
+app.use("/api/checklists", require("./routes/api/checklists"));
+app.use("/api/notifications", require("./routes/api/notifications"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT_NUMBER || 5000;
+const hostname = process.env.HOST_NAME;
 
-app.listen(PORT, () => console.log('Server started on port ' + PORT));
+// Start the server and connect to the database
+app.listen(port, hostname, () => {
+  console.log(`Server running at: http://${hostname}:${port}`);
+  Db.connectDB();
+});
