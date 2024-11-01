9bac26c875a1d3da28d86a15aed25acc36d72682
truqhieu <trunghieu2003hhh@gmail.com>
2024-11-01 16:53:06 +0700

    Xóa thư mục ngay-4-thang-9-nam-2024-tam

--- ngay-4-thang-9-nam-2024-tam/client/src/index.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/index.js
-import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
