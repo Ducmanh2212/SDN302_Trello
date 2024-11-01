6be4340ecdd96fc53c8899466994ef60d358c648
minhduc0308 <78521319+minhduc0308@users.noreply.github.com>
2024-11-01 17:03:22 +0700

    Delete ngay-4-thang-9-nam-2024-tam directory
    delete folder

--- ngay-4-thang-9-nam-2024-tam/server/models/Label.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const labelSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  color: { type: String, required: true },
-  board: { type: mongoose.Schema.Types.ObjectId, ref: "Board", required: true },
-});
-
-const Label = mongoose.model("Label", labelSchema);
