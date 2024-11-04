8ea7170cb3d8e479c8c7e6510ed6d03dda5d16d3
minhduc0308 <78521319+minhduc0308@users.noreply.github.com>
2024-11-04 12:36:54 +0700

    Update notifications.js

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -1,38 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const Notification = require('../../models/Notification');
+const auth = require("../../middleware/auth");
 
+const {
+    NotificationController
+} = require("../../controller/");
 
 // Lấy danh sách thông báo của người dùng đã đăng nhập
-router.get('/', auth, async (req, res) => {
-    try {
-        console.log("User ID:", req.user.id); // Kiểm tra ID người dùng
-        const notifications = await Notification.find({ user: req.user.id, isRead: false });
-        console.log("Notifications:", notifications); // Kiểm tra dữ liệu trả về
-        res.json(notifications);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Lỗi máy chủ');
-    }
-});
-
+router.get("/", auth, NotificationController.getUserNotifications);
 
 // Đánh dấu thông báo đã đọc
-router.put('/:id', auth, async (req, res) => {
-    try {
-        const notification = await Notification.findById(req.params.id);
-        if (!notification) return res.status(404).json({ msg: 'Không tìm thấy thông báo' });
-
-        notification.isRead = true;
-        await notification.save();
-        
-        res.json(notification);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Lỗi máy chủ');
-    }
-});
-
+router.put("/:id", auth, NotificationController.markNotificationAsRead);
 
 module.exports = router;
