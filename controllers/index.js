7e80a7ecb87d78d36769e29fa0934cc0c76c0c07
minhtam <minhtamnhgp03@gmail.com>
2024-11-05 20:48:21 +0700

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,16 @@
-const CardController = require("./card.controller");
+const BoardController = require("./boards.controller");
+const CardController = require("./cards.controller");
+const ChecklistController = require("./checklists.controller");
+const ListController = require("./lists.controller");
+const NotificationController = require("./notifications.controller");
+const AuthController = require("./auth.controller");
+const UserController = require("./users.controller");
 module.exports = {
+  BoardController,
   CardController,
+  ChecklistController,
+  ListController,
+  NotificationController,
+  AuthController,
+  UserController,
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,14 @@
+// routes/api/user.routes.js
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 const auth = require("../../middleware/auth");
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { check, validationResult } = require("express-validator");
-require("dotenv").config();
+const {
+  registerUser,
+  getUsersByEmail,
+} = require("../../controllers/user.controller");
 
-const User = require("../../models/User");
-
-// Register user
+// Route for registering a new user
 router.post(
   "/",
   [
@@ -18,70 +17,12 @@ router.post(
     check(
       "password",
       "Please enter a password with 6 or more characters"
-    ).isLength({
-      min: 6,
-    }),
+    ).isLength({ min: 6 }),
   ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { name, email, password } = req.body;
-
-    try {
-      // See if user exists
-      if (await User.findOne({ email })) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
-      }
-
-      // Register new user
-      const user = new User({
-        name,
-        email,
-        avatar: gravatar.url(email, { s: "200", r: "pg", d: "mm" }),
-        password: await bcrypt.hash(password, await bcrypt.genSalt(10)),
-      });
-
-      await user.save();
-
-      // Return jsonwebtoken
-      jwt.sign(
-        {
-          user: {
-            id: user.id,
-          },
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server error");
-    }
-  }
+  registerUser
 );
 
-// Get users with email regex
-router.get("/:input", auth, async (req, res) => {
-  try {
-    const regex = new RegExp(req.params.input, "i");
-    const users = await User.find({
-      email: regex,
-    }).select("-password");
-
-    res.json(users.filter((user) => user.id !== req.user.id));
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
-});
+// Route for getting users by email regex
+router.get("/:input", auth, getUsersByEmail);
 
 module.exports = router;
